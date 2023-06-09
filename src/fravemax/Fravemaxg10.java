63c18c913dca324c15b0ef7db9ea743bb281ae97
dariomaya <dariomaya7@hotmail.com>
2023-06-09 16:36:28 -0300

    Jugando

--- src/fravemax/Fravemaxg10.java
+++ src/fravemax/Fravemaxg10.java
@@ -1,14 +1,23 @@
-
 package fravemax;
 
+import Controlador.ClienteData;
+import Controlador.ProveedorData;
+import Modelo.Cliente;
 import Modelo.Proveedor;
-
 
 public class Fravemaxg10 {
 
-    
     public static void main(String[] args) {
-        Proveedor pro = new Proveedor();
+        ProveedorData pro = new ProveedorData();
+           Proveedor prove = new Proveedor("MarianoPuroLaburo","PabloLucero","567456",true);
+        //   pro.guardarProveedor(prov);
+        Proveedor prov = new Proveedor();
+        pro.buscarProveedor(1);
+     //   pro.modificarProveedor(prove);
+
+        /* ClienteData cli =new ClienteData();
+        Cliente clien = new Cliente("Moreira","Esteban","VillaMercedes","09876",true);
+        cli.guardarCliente(clien);*/
     }
-    
+
 }
